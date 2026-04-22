6991792304fd0f49914d83d6c3c02a15e6740264
roaogh1-ui <roaogh1@gmail.com>
2026-04-22 16:11:27 +0900

    react app

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import axios from 'axios'
+
+function App() {
+  
+  const getList = () => {
+    console.log("목록 데이터 요청...");
+
+    //로컬테스트
+    const SERVER_URL = "http://localhost:3000/api/list";
+
+    axios.get(SERVER_URL)
+    .then((res) =>{
+      console.log("응답받은 데이터>>", res.data);
+    });
+  }
+
+  return (
+    <>
+      <h1>NCP 배포 서비스</h1>
+      <button onClick={getList}>목록조회</button>   
+    </>
+  )
+}
+
+export default App
