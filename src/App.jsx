b4646126b06e1635595eaddd223a1c8821d159b5
roaogh1-ui <roaogh1@gmail.com>
2026-04-23 10:37:49 +0900

    목록조회 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,69 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Container from 'react-bootstrap/Container';
+import Table from 'react-bootstrap/Table';
+
 import axios from 'axios'
+import { useState } from 'react';
 
 function App() {
+
+  // node서버에서 요청한 정보를 보관하는 state 생성
+  const [users, setUsers] = useState([]);
   
   const getList = () => {
     console.log("목록 데이터 요청...");
 
     //로컬테스트
-    //const SERVER_URL = "http://localhost:3000/api/list";
+    const SERVER_URL = "http://localhost:3000/users/list";
 
     //배포용
-    const SERVER_URL = "/api/list";
+    // const SERVER_URL = "/users/list";
 
     axios.get(SERVER_URL)
     .then((res) =>{
       console.log("응답받은 데이터>>", res.data);
+
+      //setUsers()함수로 state 변수에 저장
+      setUsers(res.data);
     });
   }
 
   return (
     <>
+      <Container>
+
       <h1>NCP 배포 서비스</h1>
-      <button onClick={getList}>목록조회</button>   
+      <button onClick={getList}>목록조회</button>
+
+      <Table striped>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>ID</th>
+          <th>PASSWORD</th>
+          <th>NICKNAME</th>
+        </tr>
+      </thead>
+      <tbody>
+        {/* node서버로부터 받은 목록데이터를 map()로 출력 */}
+        {/*<tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>*/}
+        { users.map((user, index) => (
+              <tr key={index}>
+                <td>{index + 1}</td>
+                <td>{user.id}</td>
+                <td>{user.pw}</td>
+                <td>{user.nick}</td>
+              </tr>
+            ))}
+        
+      </tbody>
+    </Table>   
+    </Container>
     </>
   )
 }
